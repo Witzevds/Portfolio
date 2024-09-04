b85d6f98f0dff789b96a337d5f4d874624849bc4
Witzevds <witzvand@student.arteveldehs.be>
2024-09-04 22:20:15 +0200

    hero design almost done

---
+++ src/Hero.jsx
@@ -0,0 +1,17 @@
+import heroVideo from "./assets/hero-video.mp4";
+
+function Hero() {
+  return (
+    <>
+      <section className="hero">
+        <video className="hero__video" src={heroVideo} autoPlay muted loop></video>
+        <div className="hero__text">
+        <h1 className="hero__text--title">Witze</h1>
+        <h2 className="hero__text--content">Web-Design & Web Development</h2>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,6 +1,22 @@
- function Navigation() {
-
-  return <nav>hey</nav>
+function Navigation() {
+  return (
+    <>
+      <nav className="navigation">
+        <ul className="navigation__list">
+          <div className="navigation__left">
+            <li className="navigation__left--logo">
+              <a href="/">Witze</a>
+            </li>
+          </div>
+          <div className="navigation__right">
+            <li className="navigation__right--text"> <a href="/"> Contact</a></li>
+            <li className="navigation__right--text"> <a href="/">Work</a> </li>
+            <li className="navigation__right--text"> <a href="/">About</a> </li>
+          </div>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
-export default Navigation
+export default Navigation;
