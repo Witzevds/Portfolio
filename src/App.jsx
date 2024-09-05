ac2d79689c6fc8b527dcb035613ec615e898434f
Witzevds <witzvand@student.arteveldehs.be>
2024-09-05 13:58:06 +0200

    fixed the lagginess

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './Hero'
 import './css/App.css'
 import Navigation from './Navigation'
 import Work from './Work'
+import About from './About'
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navigation></Navigation>
       <Hero></Hero>
       <Work></Work>
+      <About></About>
     </>
   )
 }
--- src/Work.jsx
+++ src/Work.jsx
@@ -1,45 +1,43 @@
-import Card from './Card';  
-import neonArrow from './assets/bright-neon-colors-illuminated-arrow.jpg';
-import gentseFeesten from './assets/projects/gentse-feesten.png';
-import studioClean from './assets/projects/studio-clean.png';
-import clar from './assets/projects/clar.png';
-import recipes from './assets/projects/recipes.png';
-import './css/Work.css';
+import Card from "./Card";
+import neonArrow from "./assets/bright-neon-colors-illuminated-arrow.jpg";
+import gentseFeesten from "./assets/projects/gentse-feesten.png";
+import studioClean from "./assets/projects/studio-clean.png";
+import clar from "./assets/projects/clar.png";
+import recipes from "./assets/projects/recipes.png";
+import "./css/Work.css";
 function Work() {
   return (
     <section className="work">
-      <h2 className='work__title flicker'>Work</h2>
+      <h2 className="work__title flicker">Work</h2>
 
-      <div className='work__cards'>
-      <Card 
-  img={gentseFeesten} 
-  link="https://pgmgent-atwork-1.github.io/opdracht-2-gentse-feesten-Witzevds/" 
-  title="Gentse Feesten" 
-  description="A school project where we recreated the Gentse Feesten 2023 website, focusing on design and functionality." 
-/>
+      <div className="work__cards">
+        <Card
+          img={gentseFeesten}
+          link="https://pgmgent-atwork-1.github.io/opdracht-2-gentse-feesten-Witzevds/"
+          title="Gentse Feesten"
+          description="A school project where we recreated the Gentse Feesten 2023 website, focusing on design and functionality. (Mobile still in progress))"
+        />
 
-<Card 
-  img={clar} 
-  link="https://witzevds.github.io/clar-copy/" 
-  title="Studio Clean" 
-  description="A school assignment introducing GSAP for animations, where we built a clean and dynamic website interface." 
-/>
+        <Card
+          img={clar}
+          link="https://witzevds.github.io/clar-copy/"
+          title="Clar"
+          description="A school assignment introducing GSAP for animations, where we built a clean and dynamic website interface."
+        />
 
-<Card 
-  img={recipes} 
-  title="Recipe App" 
-  description="A web application project that allows users to create, edit, and delete recipes. The server backend was built using JSON." 
-/>
+        <Card
+          img={recipes}
+          title="Recipe App"
+          description="A web application project that allows users to create, edit, and delete recipes. The server backend was built using JSON."
+        />
 
-<Card 
-  img={studioClean} 
-  title="Studio Clean" 
-  description="This project involved using Eleventy, a static site generator, to easily create and manage content across multiple pages." 
-/>
-
+        <Card
+          img={studioClean}
+          title="Studio Clean"
+          description="This project involved using Eleventy, a static site generator, to easily create and manage content across multiple pages."
+        />
       </div>
 
- 
     </section>
   );
 }
