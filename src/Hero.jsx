d4f5d8c8e4741119d1fe4e594d57d9ca7a9e6e9a
Witzevds <witzvand@student.arteveldehs.be>
2024-09-06 01:19:01 +0200

    hero mobile working?

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,11 @@
 import heroVideo from "./assets/hero-video.mp4";
-
+import test from "./assets/static-hero.png"
 function Hero() {
   return (
     <>
       <section className="hero">
-        <video className="hero__video" src={heroVideo} autoPlay muted loop type="video/mp4"></video>
+        <video className="hero__video video" src={heroVideo} autoPlay muted loop type="video/mp4"></video>
+        <img className="image" src={test} alt="" />
         <div className="hero__text">
         <h1 className="hero__text--title">Witze</h1>
         <h2 className="hero__text--content">Web-Design & Web Development</h2>
